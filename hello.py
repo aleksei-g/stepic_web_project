fd7ed4884d3669ad33be35fff74aca080f4050b6
aleksei-g <lexa_prorock@mail.ru>
2016-03-02 20:15:00 +0300

    small changes

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
-from cgi import parse_qs
- 
-def app(environ, start_response):
-    start_response('200 OK', [('Content-type', 'text/plain')])
-    qs = parse_qs(environ['QUERY_STRING'])
-    return ['%s=%s\n' %(c,d) for c in qs for d in qs[c] ]
+#from cgi import parse_qs
+# 
+#def app(environ, start_response):
+#    start_response('200 OK', [('Content-type', 'text/plain')])
+#    qs = parse_qs(environ['QUERY_STRING'])
+#    return ['%s=%s\n' %(c,d) for c in qs for d in qs[c] ]
    
+import urlparse
+
+def app(environ, start_response):
+	qs = urlparse.parse_qs(environ['QUERY_STRING'])
+	start_response('200 OK', [('Content-type', 'text/plain')])
+	return ['%s=%s\n' %(c,d) for c in qs for d in qs[c] ]
