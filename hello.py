c7aee9d1ca56d259d1926875ac53001032446400
aleksei-g <lexa_prorock@mail.ru>
2016-03-02 21:10:10 +0300

    correct solution

--- hello.py
+++ hello.py
@@ -5,9 +5,19 @@
 #    qs = parse_qs(environ['QUERY_STRING'])
 #    return ['%s=%s\n' %(c,d) for c in qs for d in qs[c] ]
    
-import urlparse
+##
+#import urlparse
+#
+#def app(environ, start_response):
+#	qs = urlparse.parse_qs(environ['QUERY_STRING'])
+#	start_response('200 OK', [('Content-type', 'text/plain')])
+#	return ['%s=%s\n' %(c,d) for c in qs for d in qs[c] ]
 
+#import urlparse
+
+##
 def app(environ, start_response):
-	qs = urlparse.parse_qs(environ['QUERY_STRING'])
+	st = environ['QUERY_STRING']
+	qs = st.split('&')
 	start_response('200 OK', [('Content-type', 'text/plain')])
-	return ['%s=%s\n' %(c,d) for c in qs for d in qs[c] ]
+	return ['%s\n' %(c) for c in qs]	
