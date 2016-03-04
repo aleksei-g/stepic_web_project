2714d4f707f13042f4942260580b5de6039dde6a
aleksei-g <lexa_prorock@mail.ru>
2016-03-04 21:37:11 +0300

    add django

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^/|ask/|login/|new/|popular/|singup/|question/<\d+>/', 'qa.views.test', name='test'),
+    url(r'^|ask/|login/|new/|popular/|singup/|question/<\d+>/', 'qa.views.test', name='test'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
